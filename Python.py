4511415cec0d7a59f898d1087191e06609b9a0b6
Abdullah <princeabudi@gmail.com>
2015-09-23 16:38:12 +0530

    Add Average making program

--- Python.py
+++ Python.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+m=[0,0,0,0,0,0,0]
+summ=0
+for a in range(0,len(m)):
+        m[a]=int(input("Enter number "+str(a+1)+": "))
+        summ+=m[a]
+avg=summ/len(m)
+
+print("Average is "+str(avg))
